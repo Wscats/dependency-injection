6b9fa9ec95485317e53fb78ca174b8644fdd5f5e
wscats <enoyao@tencent.com>
2021-08-01 23:40:35 +0800

    hoc
    hoc

--- dist/src/descriptors.d.ts
+++
@@ -1,7 +0,0 @@
-import { Ctor } from './typings/type';
-export declare class SyncDescriptor<T> {
-    readonly ctor: Ctor<T>;
-    readonly staticArguments: any[];
-    readonly supportsDelayedInstantiation: boolean;
-    constructor(ctor: Ctor<T>, staticArguments?: any[], supportsDelayedInstantiation?: boolean);
-}
--- dist/src/extension.d.ts
+++
@@ -1,4 +0,0 @@
-import { SyncDescriptor } from './descriptors';
-import { Ctor, ServiceIdentifier } from './typings/type';
-export declare function registerSingleton<T>(id: ServiceIdentifier<T>, ctor: Ctor<T>, supportsDelayedInstantiation?: boolean): void;
-export declare function getSingletonServiceDescriptors(): [ServiceIdentifier<any>, SyncDescriptor<any>][];
